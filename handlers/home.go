71b9dd0f231654c1c16bb6a39886bac338862079
EAMR22 <elvis1042.rodriguez@gmail.com>
2022-07-01 22:46:43 -0600

    Creando el primer endpoint que es el home

---
+++ handlers/home.go
@@ -0,0 +1,25 @@
+// Este handler se va a encargar de procesar la peticion de la ruta principal.
+package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"platzi.com/go/rest-ws/server"
+)
+
+type HomeResponse struct {
+	Message string `json:"message"`
+	Status  bool   `json:"status"`
+}
+
+func HomeHandler(s server.Server) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("COntent-Type", "application/json") // Le dice al cliente que lo que le esta respondiendo es un json.
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(HomeResponse{
+			Message: "Welcome to Platzi Go",
+			Status:  true,
+		})
+	}
+}
---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
+	"platzi.com/go/rest-ws/handlers"
+	"platzi.com/go/rest-ws/server"
+)
+
+func main() {
+	err := godotenv.Load(".env") // godotenv es una de las dependencias que le instalamos.
+
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	PORT := os.Getenv("PORT") // Las variables tienen que ser inguales a las del archivo .env
+	JWT_SECRET := os.Getenv("JWT_SECRET")
+	DATABASE_URL := os.Getenv("DATABASE_URL")
+
+	s, err := server.NewServer(context.Background(), &server.Config{
+		JWTSecret:   JWT_SECRET, // A la izquierda van los valores del struct de server.go
+		Port:        PORT,       // A la derecha los valores del archivo .env
+		DatabaseUrl: DATABASE_URL,
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	s.Start(BindRoutes)
+}
+
+func BindRoutes(s server.Server, r *mux.Router) {
+	r.HandleFunc("/", handlers.HomeHandler(s)).Methods(http.MethodGet) // Es el primer endpoint.
+}
